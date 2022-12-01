880508ee8ad6586d0641ec2ca97e1c1cfc3922f8
PetarR21 <petar.r.rakocevic@gmail.com>
2022-12-01 12:46:51 +0100

    commit

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,7 +4,23 @@ const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
-  const [feedback, setFeedback] = useState([]);
+  const [feedback, setFeedback] = useState([
+    {
+      id: '1',
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: '2',
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: '3',
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
